31e7370d6484d615c110671cfc41269734443491
yongun2 <76439014+yongun2@users.noreply.github.com>
2025-06-25 21:43:03 +0900

    Time: 354 ms (56.8%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,14 +1,6 @@
 -- Write your PostgreSQL query statement below
-WITH AUTHOR AS (
-    SELECT article_id
-         , author_id
-      FROM Views
-     GROUP BY article_id, author_id
-)
-SELECT DISTINCT a.author_id AS id
-  FROM Views v
-  JOIN AUTHOR a 
-    ON v.article_id = a.article_id
-   AND v.author_id  = a.author_id
- WHERE v.viewer_id  = v.author_id
- ORDER BY id
+SELECT author_id AS id
+  FROM Views
+ WHERE author_id = viewer_id
+ GROUP BY author_id, viewer_id
+ HAVING COUNT(1) >= 1
